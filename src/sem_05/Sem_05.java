7b10515f34bbbf81ff13bca36145e1e3cfd87456
Adriana Urbina <n00266076@upn.pe>
2025-04-29 16:45:30 -0500

    Guardado demo

---
+++ src/sem_05/Sem_05.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sem_05;
+
+/**
+ *
+ * @author USURIO
+ */
+public class Sem_05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Estudiantes p=new Estudiantes();
+        p.Registrar("Laura", "Valencia");
+        p.Registrar(2565252, "lau200@gmail.com");
+        p.Registrar(32562525, 5);
+    }
+    
+}
+
+
